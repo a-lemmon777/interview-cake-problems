2bae59ef3d168eab09864ff513cf6a6588ee131a
Aaron Lemmon <lemmo031@morris.umn.edu>
2016-07-27 23:22:45 -0700

    Added some implementation structure for find_a_duplicate_space_edition.

--- find_a_duplicate_space_edition/FindDuplicate.java
+++ find_a_duplicate_space_edition/FindDuplicate.java
@@ -4,7 +4,26 @@ package find_a_duplicate_space_edition;
  * Created by a.lemmon777 on 7/27/2016. Original problem: https://www.interviewcake.com/question/java/find-duplicate-optimize-for-space
  */
 public class FindDuplicate {
-    public static int findDuplicate(int[] array) {
-        return 0;
+    // Written to use constant space and better than n^2 time.
+    public static int findDuplicate(final int[] array) {
+        // Turn the array into a min-heap.
+        transformToMinHeap(array);
+        int previousValue = removeFromMinHeap(array, array.length - 1);
+        for (int count = 1; count < array.length; ++count) {
+            final int currentValue = removeFromMinHeap(array, array.length - 1 - count);
+            if (previousValue == currentValue) {
+                return currentValue;
+            }
+            previousValue = currentValue;
+        }
+        // No duplicates found.
+        return -1;
+    }
+
+    private static void transformToMinHeap(final int[] array) {
+    }
+
+    private static int removeFromMinHeap(final int[] array, final int lastIndexOfHeap) {
+        return -1;
     }
 }
